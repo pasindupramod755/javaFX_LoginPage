299e0717e112dfc325eb57f8c36d3d6853d60478
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-24 19:19:43 +0530

    UI connet java with ID

--- src/main/java/contraller/LoginContrall.java
+++ src/main/java/contraller/LoginContrall.java
@@ -1,4 +1,15 @@
 package contraller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginContrall {
+    public TextField txtUser;
+    public PasswordField txtPassword;
+
+    public void btnLoginAction(ActionEvent e) {
+        System.out.println("User name "+txtUser.getText());
+        System.out.println("Password "+txtPassword.getText());
+    }
 }
