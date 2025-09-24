dfecf4c21e40ac72dde1f580fba102b3bc5419bb
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-24 20:00:02 +0530

    user name and password valid

--- src/main/java/contraller/LoginContrall.java
+++ src/main/java/contraller/LoginContrall.java
@@ -1,8 +1,11 @@
 package contraller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import javax.swing.*;
 
 public class LoginContrall {
     public TextField txtUser;
@@ -11,5 +14,16 @@ public class LoginContrall {
     public void btnLoginAction(ActionEvent e) {
         System.out.println("User name "+txtUser.getText());
         System.out.println("Password "+txtPassword.getText());
+        if (txtUser.getText() == ""){
+            Alert a1 = new Alert(Alert.AlertType.INFORMATION);
+            a1.setHeaderText("Input Your UserName!");
+            a1.show();
+            return;
+        }
+        if (txtPassword.getText() == ""){
+            Alert a1 = new Alert(Alert.AlertType.INFORMATION);
+            a1.setHeaderText("Input Your Password!");
+            a1.show();
+        }
     }
 }
